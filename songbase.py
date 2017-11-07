413a4f7c90ca56325037d99efa19cf44fafdf815
Harry Wang <harryjwang@gmail.com>
2017-11-07 13:26:20 -0500

    first flask demo

---
+++ songbase.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<h1>hello world!!!!!</h1>'
+
+
+@app.route('/user/<string:name>/')
+def get_user(name):
+    return '<h1>hello %s your age is %d</h1>' % (name, 3)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
